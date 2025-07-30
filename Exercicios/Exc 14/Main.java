60f255e4161dd1f5554197e9736694a395a54a78
Albert <72774907+AlbertSimaoS@users.noreply.github.com>
2025-07-30 13:59:35 -0300

    Create Main.java

---
+++ Exercicios/Exc 14/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        ConversorTemperaturaPadrao temperatura = new ConversorTemperaturaPadrao();
+        temperatura.celsiusParaFahrenheit(25);
+        temperatura.fahrenheitParaCelsius(75);
+    }
+}
