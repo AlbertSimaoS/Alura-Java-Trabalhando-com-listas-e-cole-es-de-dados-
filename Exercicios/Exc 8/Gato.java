45db171bcdb4929b9b78d1d10fa8ca856947c60e
Albert <72774907+AlbertSimaoS@users.noreply.github.com>
2025-07-30 13:33:11 -0300

    Create Gato.java

---
+++ Exercicios/Exc 8/Gato.java
@@ -0,0 +1,10 @@
+public class Gato extends Animal{
+    @Override
+    public void emitirSom() {
+        System.out.println("Miau, miau");
+    }
+
+    public void arranharMoveis(){
+        System.out.println("Gato arranha imoveis");
+    }
+}
