1cf29b811229c4fb64136ce217a926fdf3b2f461
Albert <72774907+AlbertSimaoS@users.noreply.github.com>
2025-07-30 14:02:45 -0300

    Create Titulo.java

---
+++ Exercicios/Exc 18/Titulo.java
@@ -0,0 +1,12 @@
+public class Titulo implements Comparable<Titulo> {
+    public String nome;
+
+    public Titulo(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public int compareTo(Titulo outroTitulo) {
+        return this.nome.compareTo(outroTitulo.nome);
+    }
+}
